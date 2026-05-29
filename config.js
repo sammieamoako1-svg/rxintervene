0f6624101aab8cbf4a289bb9d10c103752e8151b
sammieamoako1-svg <sammieamoako1@gmail.com>
2026-05-29 23:44:38 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
- export const env = {
-    FIREBASE_API_KEY: "AIzaSyYourFreshFirebaseKeyHere",
-    GEMINI_API_KEY: "AIzaSyYourFreshGeminiKeyHere"
+export const env = {
+    FIREBASE_API_KEY: "AIzaSyDIQgRQm5GTUWKbPWmqc_c62mDAB6JETJs", 
+    GEMINI_API_KEY: "AIzaSyANCStxyHo879glhuzYTzvazrB-64JUZkY"
 };
